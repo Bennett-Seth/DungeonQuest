054122fda3340aad7e05c1b81691c099abe14e21
parrdyl <parrdyl@DESKTOP-ESQ9VGU>
2017-05-12 15:43:02 -0600

    5/12/2017 - Added Location, Map and Actor classes DP

---
+++ DungeonQuest/src/byui/cit260/DungeonQuest/model/Actor.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.DungeonQuest.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author parrdyl
+ */
+public class Actor implements Serializable{
+    
+    // class instance variables
+    private String name; 
+    private String description;
+    private double coordinants; 
+    private String status;
+    private double strength;
+
+    public Actor() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getCoordinants() {
+        return coordinants;
+    }
+
+    public void setCoordinants(double coordinants) {
+        this.coordinants = coordinants;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getStrength() {
+        return strength;
+    }
+
+    public void setStrength(double strength) {
+        this.strength = strength;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.name);
+        hash = 37 * hash + Objects.hashCode(this.description);
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.coordinants) ^ (Double.doubleToLongBits(this.coordinants) >>> 32));
+        hash = 37 * hash + Objects.hashCode(this.status);
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.strength) ^ (Double.doubleToLongBits(this.strength) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Actor other = (Actor) obj;
+        if (Double.doubleToLongBits(this.coordinants) != Double.doubleToLongBits(other.coordinants)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.strength) != Double.doubleToLongBits(other.strength)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        if (!Objects.equals(this.status, other.status)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" + "name=" + name + ", description=" + description + ", coordinants=" + coordinants + ", status=" + status + ", strength=" + strength + '}';
+    }
+    
+}
