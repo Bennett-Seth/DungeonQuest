6418cdc8f83053b2115824f55d1a90344da0334c
Seth <Seth@Stratagar>
2017-05-12 12:01:26 -0700

    5/10/17 Main file which tests my Inventory and Game classes for DungeonQuest

--- DungeonQuest/src/dungeonquest/DungeonQuest.java
+++ DungeonQuest/src/dungeonquest/DungeonQuest.java
@@ -5,6 +5,8 @@
  */
 package dungeonquest;
 
+import byui.cit260.DungeonQuest.model.Game;
+import byui.cit260.DungeonQuest.model.Inventory;
 import byui.cit260.DungeonQuest.model.Player;
 
 /**
@@ -19,13 +21,32 @@ public class DungeonQuest {
     public static void main(String[] args) {
        
         Player playerOne = new Player();
-    
+        
         playerOne.setName("Seth");
         playerOne.setStrength(0);
-    
+        
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
-                
+    
+        Inventory sword = new Inventory();
+        
+        sword.setInventoryItem("Sword");
+        sword.setAmount(1);
+        sword.setItemDescription("The point end goes into the other man.");
+        sword.setItemLevel(5);
+        sword.setQualityInStock(1);
+    
+        String swordInfo = sword.toString();
+        System.out.println(swordInfo);
+    
+        Game newGame = new Game();
+        
+        newGame.setNoPlayer(1);
+        
+        String newGameInfo = newGame.toString();
+        System.out.println(newGameInfo);
+        
+        
     }
     
 }
