920053b723ddbf644a92e2d0bfee5b7a23048e82
Seth <Seth@Stratagar>
2017-07-13 09:13:07 -0700

    Merge origin/master
    Conflicts:
    	DungeonQuest/src/byui/cit260/DungeonQuest/view/GameMenuView.java
    	DungeonQuest/src/byui/cit260/DungeonQuest/view/InventoryView.java

--- DungeonQuest/src/byui/cit260/DungeonQuest/control/InventoryControl.java
+++ DungeonQuest/src/byui/cit260/DungeonQuest/control/InventoryControl.java
@@ -24,140 +24,140 @@ public class InventoryControl {
          Inventory bronzeDagger = new Inventory();
          bronzeDagger.setInventoryItem("Bronze Dagger");
          bronzeDagger.setItemDescription("An antique, but it can still cut");
-         bronzeDagger.setItemLevel(3);
-         bronzeDagger.setAmount(0);
+         bronzeDagger.setItemLevel(10);
+         bronzeDagger.setAmount(1);
          inventory[Item.bronzeDagger.ordinal()] = bronzeDagger;
          
          Inventory steelDagger = new Inventory();
          steelDagger.setInventoryItem("Steel Dagger");
          steelDagger.setItemDescription("A sturdy blade");
-         steelDagger.setItemLevel(5);
+         steelDagger.setItemLevel(12);
          steelDagger.setAmount(0);
          inventory[Item.steelDagger.ordinal()] = steelDagger;
          
          Inventory mithrilDagger = new Inventory();
          mithrilDagger.setInventoryItem("Mithril Dagger");
          mithrilDagger.setItemDescription("A real blade for killing real monsters");
-         mithrilDagger.setItemLevel(9);
+         mithrilDagger.setItemLevel(14);
          mithrilDagger.setAmount(0);
          inventory[Item.mithrilDagger.ordinal()] = mithrilDagger;
          
          Inventory rustySword = new Inventory();
          rustySword.setInventoryItem("Rusty Sword");
          rustySword.setItemDescription("An old, weathered weapon");
-         rustySword.setItemLevel(5);
+         rustySword.setItemLevel(12);
          rustySword.setAmount(0);
          inventory[Item.rustySword.ordinal()] = rustySword;
          
          Inventory polishedSword = new Inventory();
          polishedSword.setInventoryItem("Polished Sword");
          polishedSword.setItemDescription("A weapon for a real adventurer.");
-         polishedSword.setItemLevel(9);
+         polishedSword.setItemLevel(14);
          polishedSword.setAmount(0);
          inventory[Item.polishedSword.ordinal()] = polishedSword;
          
          Inventory mithrilSword = new Inventory();
          mithrilSword.setInventoryItem("Mitril Sword");
          mithrilSword.setItemDescription("A real blade for killing real monsters");
-         mithrilSword.setItemLevel(13);
+         mithrilSword.setItemLevel(16);
          mithrilSword.setAmount(0);
          inventory[Item.mithrilSword.ordinal()] = mithrilSword;
          
          Inventory weatheredAxe = new Inventory();
          weatheredAxe.setInventoryItem("weathered Axe");
          weatheredAxe.setItemDescription("Good for straw, logs and skulls.");
-         weatheredAxe.setItemLevel(6);
+         weatheredAxe.setItemLevel(14);
          weatheredAxe.setAmount(0);
          inventory[Item.weatheredAxe.ordinal()] = weatheredAxe;
          
          Inventory sturdyAxe = new Inventory();
          sturdyAxe.setInventoryItem("Sturdy Axe");
          sturdyAxe.setItemDescription("When it doubt, start chopping.");
-         sturdyAxe.setItemLevel(10);
+         sturdyAxe.setItemLevel(16);
          sturdyAxe.setAmount(0);
          inventory[Item.sturdyAxe.ordinal()] = sturdyAxe;
          
          Inventory vikingAxe = new Inventory();
          vikingAxe.setInventoryItem("Viking Axe");
          vikingAxe.setItemDescription("A real axe, for slaying real monsters.");
-         vikingAxe.setItemLevel(14);
+         vikingAxe.setItemLevel(18);
          vikingAxe.setAmount(0);
          inventory[Item.vikingAxe.ordinal()] = vikingAxe;
          
          Inventory holySpear = new Inventory();
          holySpear.setInventoryItem("Holy Spear");
          holySpear.setItemDescription("God blessed for destroying monsters.");
-         holySpear.setItemLevel(30);
+         holySpear.setItemLevel(35);
          holySpear.setAmount(0);
          inventory[Item.holySpear.ordinal()] = holySpear;
          
          Inventory tatteredLeatherArmor = new Inventory();
          tatteredLeatherArmor.setInventoryItem("Tattered Leather Armor");
          tatteredLeatherArmor.setItemDescription("Battered, but still usable.");
-         tatteredLeatherArmor.setItemLevel(3);
-         tatteredLeatherArmor.setAmount(0);
+         tatteredLeatherArmor.setItemLevel(10);
+         tatteredLeatherArmor.setAmount(1);
          inventory[Item.tatteredLeatherArmor.ordinal()] = tatteredLeatherArmor;
          
          Inventory sturdyLeatherArmor = new Inventory();
          sturdyLeatherArmor.setInventoryItem("Sturdy Leather Armor");
          sturdyLeatherArmor.setItemDescription("Flexible and strong armor.");
-         sturdyLeatherArmor.setItemLevel(5);
+         sturdyLeatherArmor.setItemLevel(12);
          sturdyLeatherArmor.setAmount(0);
          inventory[Item.sturdyLeatherArmor.ordinal()] = sturdyLeatherArmor;
          
          Inventory polishedLeatherArmor = new Inventory();
          polishedLeatherArmor.setInventoryItem("Polished Leather Armor");
          polishedLeatherArmor.setItemDescription("Barely used. Great quality.");
-         polishedLeatherArmor.setItemLevel(9);
+         polishedLeatherArmor.setItemLevel(14);
          polishedLeatherArmor.setAmount(0);
          inventory[Item.polishedLeatherArmor.ordinal()] = polishedLeatherArmor;
          
          Inventory tatteredScaleMail = new Inventory();
          tatteredScaleMail.setInventoryItem("Tattered Scale Mail");
          tatteredScaleMail.setItemDescription("Strong but worn.");
-         tatteredScaleMail.setItemLevel(6);
+         tatteredScaleMail.setItemLevel(12);
          tatteredScaleMail.setAmount(0);
          inventory[Item.tatteredScaleMail.ordinal()] = tatteredScaleMail;
          
          Inventory sturdyScaleMail = new Inventory();
          sturdyScaleMail.setInventoryItem("Sturdy Scale Mail");
          sturdyScaleMail.setItemDescription("Barely used. Great quality.");
-         sturdyScaleMail.setItemLevel(10);
+         sturdyScaleMail.setItemLevel(14);
          sturdyScaleMail.setAmount(0);
          inventory[Item.sturdyScaleMail.ordinal()] = sturdyScaleMail;
          
          Inventory polishedScaleMail = new Inventory();
          polishedScaleMail.setInventoryItem("Polished Scale Mail");
          polishedScaleMail.setItemDescription("Quality armor for your chest.");
-         polishedScaleMail.setItemLevel(14);
+         polishedScaleMail.setItemLevel(16);
          polishedScaleMail.setAmount(0);
          inventory[Item.polishedScaleMail.ordinal()] = polishedLeatherArmor;
          
          Inventory tatteredPlateMail = new Inventory();
          tatteredPlateMail.setInventoryItem("Tattered Plate Mail");
          tatteredPlateMail.setItemDescription("A relic from a powerful hero.");
-         tatteredPlateMail.setItemLevel(7);
+         tatteredPlateMail.setItemLevel(14);
          tatteredPlateMail.setAmount(0);
          inventory[Item.tatteredPlateMail.ordinal()] = tatteredPlateMail;
          
          Inventory sturdyPlateMail = new Inventory();
          sturdyPlateMail.setInventoryItem("Sturdy Plate Mail");
          sturdyPlateMail.setItemDescription("Covers your whole body in thick metal.");
-         sturdyPlateMail.setItemLevel(11);
+         sturdyPlateMail.setItemLevel(16);
          sturdyPlateMail.setAmount(0);
          inventory[Item.sturdyPlateMail.ordinal()] = sturdyPlateMail;
          
          Inventory polishedPlateMail = new Inventory();
          polishedPlateMail.setInventoryItem("Polished Plate Mail");
          polishedPlateMail.setItemDescription("Become a living tank of iron and steel.");
-         polishedPlateMail.setItemLevel(15);
+         polishedPlateMail.setItemLevel(18);
          polishedPlateMail.setAmount(0);
          inventory[Item.polishedPlateMail.ordinal()] = polishedPlateMail;
          
          Inventory cloakOfLight = new Inventory();
          cloakOfLight.setInventoryItem("Cloak of Light");
          cloakOfLight.setItemDescription("The best protection comes from the Gods");
-         cloakOfLight.setItemLevel(25);
+         cloakOfLight.setItemLevel(30);
          cloakOfLight.setAmount(0);
          inventory[Item.cloakOfLight.ordinal()] = cloakOfLight;
  
